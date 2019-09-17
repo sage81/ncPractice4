ab0e34c75a302d1f4ca66d4129de283891c552d5
sage81 <yvakarchuk@gmail.com>
2019-09-17 23:50:50 +0300

    at changes

--- src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
+++ src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
@@ -41,7 +41,7 @@ public class ServiceImpl implements Service {
     public void filtrateByType(Device[] devices, String type) {
         for (int i = 0; i < devices.length; i++) {
             if (devices[i] != null) {
-                if (devices[i].getType() != type) {
+                if (!type.equals(devices[i].getType())) {
                     devices[i] = null;
                 }
             }
