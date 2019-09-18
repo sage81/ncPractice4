cad901ae58213e00a6c8890967c1a6e4b092d454
sage81 <yvakarchuk@gmail.com>
2019-09-18 23:37:39 +0300

    at changes

--- src/com/netcracker/edu/inventory/Main.java
+++ src/com/netcracker/edu/inventory/Main.java
@@ -1,5 +1,6 @@
 package com.netcracker.edu.inventory;
 
+import com.netcracker.edu.inventory.exception.DeviceValidationException;
 import com.netcracker.edu.inventory.model.Device;
 import com.netcracker.edu.inventory.model.Rack;
 import com.netcracker.edu.inventory.model.impl.*;
@@ -8,51 +9,7 @@ import com.netcracker.edu.inventory.service.impl.ServiceImpl;
 public class Main {
 
     public static void main(String[] args) {
-
-        ServiceImpl service = new ServiceImpl();
-
-        Battery b = new Battery();
-        b.setType(Battery.class.getSimpleName());
-        Router r = new Router();
-        r.setType(Router.class.getSimpleName());
-        Switch s = new Switch();
-        s.setType(Switch.class.getSimpleName());
-        WifiRouter wr = new WifiRouter();
-        wr.setType(WifiRouter.class.getSimpleName());
-        Device tn = new Battery();
-        tn.setType(null);
-
-        Device[] devices = new Device[] {null, r, b, tn, r, s, tn, null, wr, b};
-        Device[] expResult = new Device[] {null, null, null, tn, null, null, tn, null, null, null};
-
-        service.filtrateByType(devices, null);
-//        service.filtrateByType(devices, r.getClass().getSimpleName());
-//        showDevArray(devices);
-
-        System.out.println("*******************************************************************");
-        showDevArray(devices);
-
-//        RackArrayImpl rack = new RackArrayImpl(15);
-//
-//        Battery battery = new Battery(4, "Battery", "DX Global", "XX5ST", null, 1);
-//        Router router = new Router(20, "Router", "RT Manufacture", "Router1", null, 1000);
-//        Switch swch = new Switch(3, "Switch", "SW Corp", "Switch1", null, 100, 8);
-//        WifiRouter wifi = new WifiRouter(1, "WifiRouter", "Wifi Corp", "Wifi model1", null, 100,
-//                "PROT1");
-//
-//        Device[] devices = new Device[] {null, battery, router, new Battery(), swch, wifi, null};
-//
-//        ServiceImpl service = new ServiceImpl();
-//        service.sortByIN(devices);
-//
-//        showDevArray(devices);
-//
-//        service.filtrateByType(devices, null);
-//        service.sortByIN(devices);
-//
-//        System.out.println("*******************************************************************");
-//        showDevArray(devices);
-
+        DeviceValidationException v = new DeviceValidationException(null);
     }
 
     private static void showRack(Rack rack) {
